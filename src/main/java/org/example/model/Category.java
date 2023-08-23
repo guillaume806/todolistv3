0b794e5008617c0d9259b01930e9a636bee9b849
guillaume806 <guillaumebodart@outlook.fr>
2023-08-23 14:59:53 +0200

    category

--- src/main/java/org/example/model/Category.java
+++ src/main/java/org/example/model/Category.java
@@ -16,6 +16,36 @@ public class Category {
     private List<Task> tasks = new ArrayList<>();
 
 
+    public Category() {
+    }
 
+    public Category(Long id, String cname, List<Task> tasks) {
+        this.id = id;
+        Cname = cname;
+        this.tasks = tasks;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCname() {
+        return Cname;
+    }
+
+    public void setCname(String cname) {
+        Cname = cname;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
